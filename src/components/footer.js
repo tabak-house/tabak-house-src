016f90cb4bff926e307c659235fd4a947c6c1764
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2021-04-04 18:36:18 +0300

    Rework footer and navbar.

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,19 +6,15 @@ import {Link} from "gatsby";
 const Footer = () => {
   return (
     <Container>
-      <footer className={'py-5'}>
-
-        <Row noGutters>
-          <Col xs={12} sm={4}>
-            <Link to={'/'}>
-              <Image src={logo} height={55} alt={'Логотип'}/>
-            </Link>
-          </Col>
-          <Col xs={12} sm={8}>
-            <span>© {new Date().getFullYear()}, <a
-              href="https://tabak.house">Tabak.house</a></span>
-          </Col>
-        </Row>
+      <footer className={'py-5 text-center'}>
+        <p>
+          <Link to={'/'}>
+            <Image src={logo} height={55} alt={'Логотип'}/>
+          </Link>
+        </p>
+        <p>
+          <span>© {new Date().getFullYear()}, <a href="https://tabak.house">Tabak.house</a></span>
+        </p>
       </footer>
     </Container>
   )
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -6,40 +6,46 @@ import {
   Nav,
   NavDropdown,
   Image,
-  NavItem,
+  NavItem, Container, Row, Col,
 } from "react-bootstrap"
 
 const CustomNavbar = ({pageInfo}) => {
 
   return (
-    <Navbar expand={"lg"} variant={"light"}>
-      <Navbar.Brand href="/">
-        <Image src={logo} height={55} alt={'Логотип'}/>
-      </Navbar.Brand>
+    <Container>
+      <Row>
+        <Col>
+          <Navbar expand={"lg"} variant={"light"}>
+            <Navbar.Brand href="/">
+              <Image src={logo} height={55} alt={'Логотип'}/>
+            </Navbar.Brand>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Navbar.Collapse id="basic-navbar-nav" >
-        <Nav>
-          <NavItem>
-            <Nav.Link href="/">Главная</Nav.Link>
-          </NavItem>
-          {/*<NavItem>*/}
-          {/*  <NavDropdown title="Каталог" id="basic-nav-dropdown">*/}
-          {/*    <NavDropdown.Item href="/">Табак</NavDropdown.Item>*/}
-          {/*    <NavDropdown.Divider/>*/}
-          {/*    <NavDropdown.Item href="/">Кальяны</NavDropdown.Item>*/}
-          {/*    <NavDropdown.Item href="/">Колбы</NavDropdown.Item>*/}
-          {/*    <NavDropdown.Item href="/">Чаши для кальяна</NavDropdown.Item>*/}
-          {/*    <NavDropdown.Item href="/">Калауд и колпаки</NavDropdown.Item>*/}
-          {/*    <NavDropdown.Divider/>*/}
-          {/*    <NavDropdown.Item href="/">Уголь, кадило и щипцы</NavDropdown.Item>*/}
-          {/*  </NavDropdown>*/}
-          {/*</NavItem>*/}
-          <Nav.Link href="/delivery">Доставка и оплата</Nav.Link>
-          <Nav.Link href="/contact">Контакты</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav" >
+              <Nav className={'ml-auto'}>
+                <NavItem>
+                  <Nav.Link href="/">Главная</Nav.Link>
+                </NavItem>
+                {/*<NavItem>*/}
+                {/*  <NavDropdown title="Каталог" id="basic-nav-dropdown">*/}
+                {/*    <NavDropdown.Item href="/">Табак</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Divider/>*/}
+                {/*    <NavDropdown.Item href="/">Кальяны</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Item href="/">Колбы</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Item href="/">Чаши для кальяна</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Item href="/">Калауд и колпаки</NavDropdown.Item>*/}
+                {/*    <NavDropdown.Divider/>*/}
+                {/*    <NavDropdown.Item href="/">Уголь, кадило и щипцы</NavDropdown.Item>*/}
+                {/*  </NavDropdown>*/}
+                {/*</NavItem>*/}
+                <Nav.Link href="/delivery">Доставка и оплата</Nav.Link>
+                <Nav.Link href="/contact">Контакты</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
