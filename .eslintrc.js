00029aa74c6826bd869d049d7090e4bc4ff4d357
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2021-04-04 22:59:43 +0300

    Add stylelint and eslint with gatsby-specific settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,17 @@ module.exports = {
     'browser': true,
     'es2021': true,
   },
+  'parser': 'babel-eslint',
   'extends': [
+    'react-app',
     'google',
   ],
   'parserOptions': {
     'ecmaVersion': 12,
     'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+    },
   },
   'rules': {
   },
